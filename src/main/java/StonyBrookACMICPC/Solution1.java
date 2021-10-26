1472ea50e358293c74c176e68176481637de5a55
nitinvishwari <nitinvishwari@gmail.com>
2021-10-26 19:54:14 -0400

    Template updated with inputFiles added

--- src/main/java/StonyBrookACMICPC/Solution1.java
+++ src/main/java/StonyBrookACMICPC/Solution1.java
@@ -1,13 +1,18 @@
 package StonyBrookACMICPC;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Arrays;
 import java.util.Scanner;
 
 
 public class Solution1 {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws FileNotFoundException {
+//        Scanner scanner = new Scanner(System.in);
+        File text = new File("E:\\Spring Boot Course\\CompetitiveProgramming\\src\\main\\java\\StonyBrookACMICPC\\InputFiles\\input1.txt");
+        Scanner scanner = new Scanner(text);
+
         int t = scanner.nextInt();
         for(int i=0; i<t; i++) {
 
--- src/main/java/StonyBrookACMICPC/Solution8.java
+++ src/main/java/StonyBrookACMICPC/Solution8.java
@@ -1,10 +1,15 @@
 package StonyBrookACMICPC;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Solution8 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws FileNotFoundException {
+//        Scanner scanner = new Scanner(System.in);
+        File text = new File("E:\\Spring Boot Course\\CompetitiveProgramming\\src\\main\\java\\StonyBrookACMICPC\\InputFiles\\input8.txt");
+        Scanner scanner = new Scanner(text);
+
         int t = scanner.nextInt();
         for(int i=0; i<t; i++) {
 
